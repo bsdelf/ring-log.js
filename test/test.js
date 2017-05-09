542f2e6f9759a02884b9be6b14ca1c35de7059ed
Yanhui Shen <shen.elf@gmail.com>
2017-05-09 11:12:23 +0800

    Improve test

--- test/test.js
+++ test/test.js
@@ -1,68 +1,107 @@
 'use strict';
 
-const assert = require('assert');
+const fs = require('fs');
+const assert = require('chai').assert;
 const RingLog = require('../src')
 
+const LOG_PATH = 'test.log';
+
+function genPath() {
+    return `test.${Date.now().toString(36)}.log`;
+}
+
 describe('RingLog', () => {
-    describe('constructor()', () => {
-        it('should throw when filePath or maxBytes is missing', () => {
-            assert.throws(() => {
-                throw new Error('');
-            })
+    describe('#openSync()', () => {
+        let logPath;
+
+        before(() => {
+            logPath = genPath();
         });
 
-        it('should throw when filePath is not writable', () => {
-            assert.throws(() => {
-                throw new Error('');
-            })
-        });
-
-        it('should throw when maxBytes < 8', () => {
-            assert.throws(() => {
-                throw new Error('');
-            })
-        });
-    });
-
-    describe('open()', () => {
-        it('should open', () => {
+        it('create log if it does not exist', () => {
             let log = new RingLog(1024);
-            log.open('a.log');
+            log.openSync(logPath);
+            log.closeSync();
+            assert.isTrue(fs.existsSync(logPath));
+        });
+
+        after(() => {
+            fs.unlinkSync(logPath);
         });
     });
 
-    describe('append()', () => {
-        it('should append', () => {
-        });
-    });
+    describe('#pushSync()', () => {
+        let logPath;
+        let log;
 
-    describe('fetch()', () => {
-        it('should fetch', () => {
-        });
-    });
+        before(() => {
+            logPath = genPath();
+            log = new RingLog(RingLog.META_SIZE + 4 + 8 + 1);
+            log.openSync(logPath);
+        })
 
-    describe('clear()', () => {
-        it('should clear', () => {
+        it('should push if feasible', () => {
+            assert.doesNotThrow(() => {
+                log.pushSync({id:0,str:'a'});
+                log.pushSync({id:0,str:'b'});
+            });
+            assert.isTrue(log.isFull);
+            assert.isFalse(log.isEmpty);
+        });
+
+        it('should throw if not feasible', () => {
+            assert.throws(() => {
+                log.pushSync({id:0,str:'ab'});
+            });
+        });
+
+        after(() => {
+            fs.unlinkSync(logPath);
         })
     });
 
-    describe('resize()', () => {
-        it('should resize', () => {
-        });
-    });
+    describe('#shiftSync()', () => {
+        let logPath;
+        let log;
 
-    describe('forEach()', () => {
-        it('should for-each', () => {
-        });
-    });
+        let n = 26;
 
-    describe('map()', () => {
-        it('should map', () => {
-        });
-    });
+        before(() => {
+            logPath = genPath();
+            log = new RingLog(RingLog.META_SIZE + (4 + 8 + 1) * n);
+            log.openSync(logPath);
 
-    describe('reduce()', () => {
-        it('should reduce', () => {
+            assert.doesNotThrow(() => {
+                for (let id = 0; id < n; ++id) {
+                    let str = String.fromCharCode('a'.charCodeAt(0) + id);
+                    log.pushSync({id, str});
+                }
+            });
+            assert.isTrue(log.isFull);
+            assert.isFalse(log.isEmpty);
+        })
+
+        it('should shift if available', () => {
+            assert.doesNotThrow(() => {
+                for (let i = 0; i < n; ++i) {
+                    let ch = String.fromCharCode('a'.charCodeAt(0) + i);
+                    let {id, str} = log.shiftSync();
+                    assert.strictEqual(id, i);
+                    assert.strictEqual(str, ch);
+                }
+            });
+            assert.isFalse(log.isFull);
+            assert.isTrue(log.isEmpty);
         });
+
+        it('should throw if not available', () => {
+            assert.throws(() => {
+                let {id, str} = log.shiftSync();
+            });
+        });
+
+        after(() => {
+            fs.unlinkSync(logPath);
+        })
     });
 });
